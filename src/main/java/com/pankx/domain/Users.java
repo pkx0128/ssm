82c224e4571890dba2d9db2cbb54087613c711be
pkx0128 <764670547@qq.com>
2020-03-11 14:36:58 +0800

    实体类Users增加id属性

--- src/main/java/com/pankx/domain/Users.java
+++ src/main/java/com/pankx/domain/Users.java
@@ -6,8 +6,17 @@ import java.io.Serializable;
  * 账号实体类
  */
 public class Users implements Serializable {
+    private int id;//用户id
     private String userName;//用户名
     private String password;//密码
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getUserName() {
         return userName;
@@ -28,7 +37,8 @@ public class Users implements Serializable {
     @Override
     public String toString() {
         return "Users{" +
-                "userName='" + userName + '\'' +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
                 ", password='" + password + '\'' +
                 '}';
     }
